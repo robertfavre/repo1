ce1095072ee6b57dd2d26344c49ee71473a52c3e
robertfavre <bob@applicationkinetics.com>
2022-11-25 19:24:58 -0500

    test commit el

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn la React
+          Learn el React
         </a>
       </header>
     </div>
